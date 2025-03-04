93a318cc14a192690e33b384e8739c22f73b6f19
GinusHR <145678936+GinusHR@users.noreply.github.com>
2025-03-04 17:26:52 +0100

    Added timer to delete invalid keys

--- index.js
+++ index.js
@@ -22,13 +22,6 @@ try {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
-
-
-
-
 //Global middleware
 //Make sure the client is informed this webservice only sends JSON
 app.use((req, res, next) => {
@@ -59,7 +52,7 @@ app.use((req, res, next) => {
 //Generates API key
 app.post('/generateApiKeys', (req,res) => {
     const apiKey = uuidv4();
-    const expiresAt = Date.now() + 60 * 60 * 100
+    const expiresAt = Date.now() + 60 * 60 * 24000
 
     db.run('INSERT INTO keys (api_keys, expires_at) VALUES (?,?)', [apiKey, expiresAt], (err) => {
         if(err) {
@@ -72,18 +65,20 @@ app.post('/generateApiKeys', (req,res) => {
 //API key authenticator middeware
 app.use( (req,res,next) => {
     const apiKey = req.headers['x-api-key']
-
+    console.log(apiKey)
     //Checks if there is an API key. If not, error
     if (!apiKey) {
         return res.status(401).json({ error: 'API Key vereist' });
     }
 
-
-    db.run('SELECT ALL FROM keys WHERE api_keys = ? AND expires_at > ?', [apiKey, Date.now()], (err, row) => {
+    //DB query that checks time and current api key
+  db.get(`SELECT * FROM keys WHERE api_keys = ? AND expires_at > ?`, [apiKey, Date.now()], (err, row) => {
+       //Query error
         if(err) {
              return res.status(500).json({error: 'Database fout'})
         }
 
+        //If api key doesn't exist, or isn't valid
         if(!row) {
             return  res.status(401).json({error: 'Ongeldige of verlopen API key'})
         }
@@ -92,19 +87,20 @@ app.use( (req,res,next) => {
 });
 
 
-
-
-
 //Routes
 app.use('/v1/signs', signsV1);
 
 
-
-
-
-
-// Cronjob nodig met een timer die kijkt in de database en de expired keys verwijderd.
-
+// Cronjob/Timer that deletes invalid keys.
+setInterval(() => {
+    db.run('DELETE FROM keys WHERE expires_at < ?', [Date.now()], (err) => {
+        if (err) {
+            console.log('Fout bij verwijderen van key', err.message);
+        } else {
+            console.log('Key succesvol verwijderd');
+        }
+    })
+}, 5 * 60 * 1000)
 
 //Print the port to the console so we know when it's actually running
 app.listen(process.env.EXPRESS_PORT, () => {
