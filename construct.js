7cb5dbfccb99d0ca9c0997448f0574f1d82d09e0
Sander <sanderlandm@gmail.com>
2025-03-10 14:46:17 +0100

    ADD Facial expressions now added when running the constructDatabase command

--- construct.js
+++ construct.js
@@ -1,5 +1,6 @@
 import Sequelize from 'sequelize';
 import fs from 'fs';
+import Facial_expression from "./v1/models/Facial_expression.js";
 
 const sql_file_content = fs.readFileSync('./database_frame.sql', 'utf8');
 
@@ -17,6 +18,21 @@ try {
     }
 
     console.log('Successfully constructed the database')
+
+    //Add standard facial expressions to database
+    const facial_expressions = ['blij', 'boos', 'geen', 'vragend zonder mond', 'vragend met mond'];
+
+    for (const facialExpression of facial_expressions) {
+
+        const facial_expression = Facial_expression.build({
+            name: facialExpression,
+            image_path: `${facialExpression.replace(/\s/g, '-')}.png`
+        });
+
+        await facial_expression.save();
+
+    }
+
 
 } catch (error) {
     console.log(error.message)
