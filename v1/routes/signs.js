f53c59eb4f3b09220384c2885519843b14ee8e26
Sander <sanderlandm@gmail.com>
2025-03-11 11:38:52 +0100

    ADD you can now also update the linked facial expressions when updating a sign

--- v1/routes/signs.js
+++ v1/routes/signs.js
@@ -2,6 +2,7 @@ import express from 'express';
 import Sign from '../models/Sign.js';
 import facial_expression from "../models/Facial_expression.js";
 import FacialExpression from "../models/Facial_expression.js";
+import Facial_expression_sign from "../models/Facial_expression_sign.js";
 
 const router = express.Router();
 
@@ -165,12 +166,46 @@ router.put('/:id', async (req, res) => {
 
     //Build a model based on the req.body so we can validate its contents
     const postedSign = Sign.build(req.body);
+    let postedExpressions = [];
 
     //Validate the data before doing anything with the database
     try {
 
         await postedSign.validate();
         postedSign.lesson = parseInt(postedSign.lesson);
+
+        if (req.body.expressions) {
+
+            if (Array.isArray(req.body.expressions)) {
+
+                postedExpressions = req.body.expressions
+
+            } else if (Number.isSafeInteger(req.body.expressions)) {
+
+                postedExpressions.push(req.body.expressions);
+
+            } else {
+
+                res.status(400);
+                return res.json({error: 'expressions must be a number or an array of numbers'});
+
+            }
+
+            //Filter out anything that isn't a number and can't be converted to a number
+            postedExpressions = postedExpressions.filter((postExpr) => {
+                //If it's a number, just return it
+                if (Number.isSafeInteger(postExpr)) {
+                    return postExpr;
+                }
+
+                //If it can be converted into a number, return the converted number
+                if (!isNaN(parseInt(postExpr))) {
+                    return parseInt(postExpr);
+                }
+
+            });
+
+        }
 
     } catch (error) {
 
@@ -201,6 +236,50 @@ router.put('/:id', async (req, res) => {
 
         await sign.save();
 
+        //Only update the expressions if they got sent in with the request
+        if (postedExpressions !== []) {
+
+            const expressionRecords = await Facial_expression_sign.findAll({
+                where: {sign_id: sign.id},
+                attributes: ['facial_expression_id']
+            });
+
+            const expressionIds = [];
+
+            for (const expressionRecord of expressionRecords) {
+                expressionIds.push(expressionRecord.dataValues.facial_expression_id);
+            }
+
+            //Only update if things actually changed
+            if (postedExpressions !== expressionIds) {
+
+                const fetchedExpressions = []
+
+                for (const postedExpression of postedExpressions) {
+                    const expr = await FacialExpression.findByPk(postedExpression);
+
+                    if (expr) {
+                        fetchedExpressions.push(expr);
+                    } else {
+                        res.status(400);
+                        return res.json({error: `Expression with the id ${postedExpression} does not exist`})
+                    }
+                }
+
+                await sign.setFacialExpressions(fetchedExpressions);
+
+            }
+
+
+        }
+
+        //Reload the sign from the database so we can also return the facial expressions
+        await sign.reload({
+            include: [{
+                model: facial_expression,
+                through: {attributes: []}
+            }]
+        });
 
         res.status(200);
         res.json({success: true, sign: sign});
