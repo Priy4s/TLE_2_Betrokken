558fa672b44d09cbe9a126192865ab06b399cf1a
Sander <sanderlandm@gmail.com>
2025-03-12 13:36:43 +0100

    ADD Generating a key now requires the request to specify who the key is for

--- v1/routes/keys.js
+++ v1/routes/keys.js
@@ -1,19 +1,39 @@
 import express from 'express';
 import Key from '../models/Key.js';
 import {v4 as uuidv4} from "uuid";
+import User from "../models/User.js";
 
 
 const router = express.Router();
 
 
 router.post('/generateApiKeys', async (req, res) => {
+
+    //When making a new API key, a user's code must be given in the body
+    if (!req.body.userCode) {
+        res.status(400);
+        return res.json({error: "Please send a body with a field named userCode containing the receiving user's code"})
+    }
+
     try {
         const apiKey = uuidv4();
-        const expiresAt = Date.now() + 60 * 60 * 730000 * 6
+        const expiresAt = Date.now() + 60 * 60 * 730000 * 6;
+
+        const user = await User.findOne({
+            where: {code: req.body.userCode},
+            attributes: ['id'],
+            raw: true
+        });
+
+        if (!user) {
+            res.status(400);
+            return res.json({error: 'User does not exist'})
+        }
 
         const key = await Key.create({
             api_keys: apiKey,
-            expires_at: expiresAt
+            expires_at: expiresAt,
+            user_id: user.id
         });
 
         return res.status(200).json({apiKey: key.api_keys, expiresAt: new Date(expiresAt).toISOString()});
